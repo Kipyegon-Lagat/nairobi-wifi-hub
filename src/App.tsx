eafd7b6da9065b8089ebc54a9c566a19fdf47baa
Kipyegon Lagat <kipyegongithub@gmail.com>
2025-07-28 23:46:06 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ const App = () => (
             <Route path="/dashboard" element={
               <ProtectedRoute>
                 <Layout>
-                  <Dashboard />
+                  <Dashboard/>
                 </Layout>
               </ProtectedRoute>
             } />
